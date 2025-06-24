d144d0a1f94014e630867800d09b7173cd1f02e8
calculon102 <frank@pixelgerecht.de>
2025-06-24 22:01:18 +0200

    Configure and add prose to lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,1 +1,63 @@
-require('lualine').setup()
+local prose = require 'nvim-prose'
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    always_show_tabline = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+      refresh_time = 16, -- ~60fps
+      events = {
+        'WinEnter',
+        'BufEnter',
+        'BufWritePost',
+        'SessionLoadPost',
+        'FileChangedShellPost',
+        'VimResized',
+        'Filetype',
+        'CursorMoved',
+        'CursorMovedI',
+        'ModeChanged',
+      },
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {
+        { prose.word_count,   cond = prose.is_available },
+        { prose.reading_time, cond = prose.is_available },
+        'encoding',
+        'fileformat',
+        'filetype'
+    },
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
+
---
+++ after/plugin/prose.lua
@@ -0,0 +1,8 @@
+require('nvim-prose').setup {
+    wpm = 200.0,
+    filetypes = { 'markdown', 'asciidoc' },
+    placeholders = {
+        words = 'words',
+        minutes = 'min'
+    }
+}
