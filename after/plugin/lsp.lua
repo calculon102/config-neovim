afe9aa3be6b901d6ffd38064e5f3701d0a90479e
calculon102 <frank@pixelgerecht.de>
2025-08-03 19:31:52 +0200

    Reduce required lsp-modules

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -34,7 +34,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here 
   -- with the ones you want to install
-  ensure_installed = {'ts_ls', 'rust_analyzer'},
+  ensure_installed = {'rust_analyzer'},
   handlers = {
     lsp.default_setup,
   },
